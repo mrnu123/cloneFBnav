9779e1976f8d6e66a9816bf6dc02359edc6ee39b
mrnu123 <49053059+mrnu123@users.noreply.github.com>
2020-10-28 23:20:06 +0700

    change code nav bar

--- myapp/src/components/nav.js
+++ myapp/src/components/nav.js
@@ -1,65 +1,152 @@
-import React, { Component } from 'react';
-import Alert from 'react-bootstrap/Alert';
-import logo_fb from '../image/facebook_logo.svg'
-import home_btn from '../image/home.svg'
-import vdo_btn from '../image/ondemand_video.svg'
-import market_btn from '../image/storefront.svg'
-import group_btn from '../image/groups.svg'
-import game_btn from '../image/sports_esports.svg'
-import user_btn from '../image/account_circle.svg'
-import add_btn from '../image/add_circle.svg'
-import msg_btn from '../image/messenger.svg'
-import './nav.css'
-
-
-
+import '../index.css';
+import {ReactComponent as PlusIcon} from '../image/add_circle.svg'
+import {ReactComponent as BellIcon} from '../image/bell.svg'
+import {ReactComponent as MsgIcon} from '../image/messenger.svg'
+import {ReactComponent as DropDownIcon} from '../image/dropdown.svg'
+import {ReactComponent as CogIcon} from '../image/cog.svg'
+import {ReactComponent as ChevronIcon} from '../image/chevron.svg'
+import {ReactComponent as CircleIcon} from '../image/circle.svg'
+import {ReactComponent as BackIcon} from '../image/arrow.svg'
+import {ReactComponent as ItemIcon} from '../image/flash.svg'
+import {ReactComponent as FBIcon} from '../image/facebook_logo.svg'
+import {ReactComponent as HomeIcon} from '../image/home.svg'
+import {ReactComponent as VDOIcon} from '../image/ondemand_video.svg'
+import {ReactComponent as MarketIcon} from '../image/storefront.svg'
+import {ReactComponent as CommunityIcon} from '../image/groups.svg'
+import {ReactComponent as GameIcon} from '../image/sports_esports.svg'
+import React, { useState, useEffect, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
+// import {ReactComponent as add_btn} from './image/add_circle.svg'
+// import add_btn from './image/add_circle.svg'
 function nav() {
   return (
-    <nav className="navbar">
-        <a class="navbar-brand" href="#">
-            <img src={logo_fb} width="48" height="48"></img>
-        </a>
-        <form>
-            <input class="form-control mr-sm-2" type="search" placeholder="Search Facebook" aria-label="Search" />
-        </form>
-        <div class="col-2"></div>
-        {/* <div class="row"> */}
-        <div class="col">
-        <a class="navbar-brand" href="#">
-            <img src={home_btn} width="36" height="36" type="image/svg+xml" />
-        </a>
+    <Navbar >
+    {/* <NavItemLeft iconLeft={<PlusIcon />} /> */}
+
+      <Ul class_name="left">
+        <Logo icon={<FBIcon />} />
+      </Ul>
+
+      <Ul class_name="center">
+        <NavItem icon={<HomeIcon />} class_name="icon-center" class_li_name="nav-item-center" link="index.html"/>
+        <NavItem icon={<VDOIcon />} class_name="icon-center" class_li_name="nav-item-center" link="vdo.html"/>
+        <NavItem icon={<MarketIcon />} class_name="icon-center" class_li_name="nav-item-center" link="#"/>
+        <NavItem icon={<CommunityIcon />} class_name="icon-center" class_li_name="nav-item-center" link="#"/>
+        <NavItem icon={<GameIcon />} class_name="icon-center" class_li_name="nav-item-center" link="#"/>
+      </Ul>
+
+      <Ul class_name="right">
+        <NavItem icon={<PlusIcon />} class_name="icon-button" class_li_name="nav-item"/>
+        <NavItem icon={<MsgIcon />} class_name="icon-button" class_li_name="nav-item"/>
+        <NavItem icon={<BellIcon />} class_name="icon-button" class_li_name="nav-item"/>
+        <NavItem icon={<DropDownIcon />} class_name="icon-button" class_li_name="nav-item">
+          <DropdownMenu />
+        </NavItem>
+      </Ul>
+
+     
+    </Navbar>
+  );
+}
+
+function DropdownMenu(){
+
+  const [activeMenu,setActiveMenu]=useState('main');
+
+  function DropdownItem(props){
+    return(
+      <a href="#" className="menu-item" onClick={()=>props.goToMenu && setActiveMenu(props.goToMenu)}>
+        <span className="icon-left">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+  return(
+    <div className="dropdown">
+      <CSSTransition in ={activeMenu==='main'} unmountOnExit timeout={500} classNames="menu-primary">
+        <div className="menu">
+          <DropdownItem 
+            leftIcon={<CircleIcon />}>Menu Header</DropdownItem>
+          <DropdownItem 
+            leftIcon={<CogIcon />} 
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings"
+          >Clickable Button
+          </DropdownItem>
         </div>
-        <div class="col">
-        <a class="navbar-brand" href="#">
-            <img src={vdo_btn} width="36" height="36" type="image/svg+xml" />
-        </a>
+      </CSSTransition>
+
+      <CSSTransition in ={activeMenu==='settings'} 
+        unmountOnExit timeout={500} 
+        classNames="menu-secondary"
+        >
+          <div className="menu">
+            <DropdownItem 
+              leftIcon={<BackIcon />} 
+              goToMenu="main"
+            >
+              <h2>Clickable Back Button</h2>
+            </DropdownItem>
+
+            <DropdownItem
+              leftIcon={<ItemIcon />}>Item 1
+            </DropdownItem>
+
+            <DropdownItem
+              leftIcon={<ItemIcon />}>Item 2
+            </DropdownItem>
+
+            <DropdownItem
+              leftIcon={<ItemIcon />}>Item 3
+            </DropdownItem>
+
+            <DropdownItem
+              leftIcon={<ItemIcon />}>Item 4
+            </DropdownItem>
+
         </div>
-        <div class="col">
-        <a class="navbar-brand" href="#">
-            <img src={market_btn} width="36" height="36" type="image/svg+xml" />
-        </a>
-        </div>
-        <div class="col">
-        <a class="navbar-brand" href="#">
-            <img src={group_btn} width="36" height="36" type="image/svg+xml" />
-        </a>
-        </div>
-        <div class="col">
-        <a class="navbar-brand" href="#">
-            <img src={game_btn} width="36" height="36" type="image/svg+xml" />
-        </a>
-        </div>
-        <div class="col-2"></div>
-        <a class="navbar-brand" href="#">
-            <img src={user_btn} width="36" height="36" type="image/svg+xml" />User
-        </a>
-        <a class="navbar-brand" href="#">
-            <img src={add_btn} width="48" height="48" type="image/svg+xml" />
-        </a>
-        <a class="navbar-brand" href="#">
-            <img src={msg_btn} width="40" height="40" type="image/svg+xml" />
-        </a>
-    </nav>
+      </CSSTransition>
+    </div>
   )
 }
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+          {props.children}
+    </nav>
+  );
+}
+
+function Ul(props){
+  return(
+    <ul className={props.class_name}>
+    {props.children}
+  </ul>
+  );
+}
+
+function NavItem(props){
+  const[open,setOpen]=useState(false);
+  return(
+    <li className={props.class_li_name}>
+      <a href={props.link} className={props.class_name} onClick={()=>setOpen(!open)}>
+        {props.icon}
+      </a>
+      {open && props.children}
+    </li>
+   
+  )
+}
+function Logo(props){
+  return(
+    <li class="logo">
+    <a href="#">
+   {props.icon}
+   </a>
+   </li>
+  )
+}
+
 export default nav;
